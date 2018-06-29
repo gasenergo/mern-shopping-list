b5c43a92bf668e84133b81c8eeb9c9a49d6267bb
Denis <gazenergodenis@gmail.com>
2018-06-29 22:58:57 +0400

    server configured

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://Denis:StaRWarS12@ds121461.mlab.com:21461/shopping'
+}
---
+++ models/Item.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create schema
+const ItemSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
